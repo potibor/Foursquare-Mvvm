5e8d85c98c50c1811365338e54c246cd7479d630
Hasan Ozan AL <>
2021-07-19 22:23:09 +0300

    [ozanal] [network module is added] [ozanal] [home fragment is created]

---
+++ app/src/main/java/com/example/foursquaremvvm/di/NetworkModule.kt
@@ -0,0 +1,76 @@
+package com.example.foursquaremvvm.di
+
+import android.app.Application
+import android.content.Context
+import com.example.foursquaremvvm.util.Constants
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder().baseUrl(Constants.BASE_URL).client(client)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+    }
+
+    private val READ_TIMEOUT = 60
+    private val WRITE_TIMEOUT = 60
+    private val CONNECTION_TIMEOUT = 60
+    private val CACHE_SIZE_BYTES = 10 * 1024 * 1024L // 10 MB
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        headerInterceptor: Interceptor,
+        cache: Cache
+    ): OkHttpClient {
+        val okHttpClientBuilder = OkHttpClient().newBuilder()
+        okHttpClientBuilder.connectTimeout(CONNECTION_TIMEOUT.toLong(), TimeUnit.SECONDS)
+        okHttpClientBuilder.readTimeout(READ_TIMEOUT.toLong(), TimeUnit.SECONDS)
+        okHttpClientBuilder.writeTimeout(WRITE_TIMEOUT.toLong(), TimeUnit.SECONDS)
+        okHttpClientBuilder.cache(cache)
+        okHttpClientBuilder.addInterceptor(headerInterceptor)
+        return okHttpClientBuilder.build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideHeaderInterceptor(): Interceptor {
+        return Interceptor {
+            val requestBuilder = it.request().newBuilder()
+            it.proceed(requestBuilder.build())
+        }
+    }
+
+
+    @Provides
+    @Singleton
+    internal fun provideCache(context: Context): Cache {
+        val httpCacheDirectory = File(context.cacheDir.absolutePath, "HttpCache")
+        return Cache(httpCacheDirectory, CACHE_SIZE_BYTES)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideContext(application: Application): Context {
+        return application.applicationContext
+    }
+}
---
+++ app/src/main/java/com/example/foursquaremvvm/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.foursquaremvvm.util
+
+object Constants {
+
+    const val BASE_URL = ""
+}
